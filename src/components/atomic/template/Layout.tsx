5624656fa794b76fd1a58473288c48252a3abb7a
Mimu-salon <tmimura0@gmail.com>
2021-12-13 15:35:38 +0900

    🐛 Fix layout

--- src/components/atomic/template/Layout.tsx
+++ src/components/atomic/template/Layout.tsx
@@ -18,7 +18,7 @@ export const Layout: VFC<Props> = memo((props) => {
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content=" width=device-width, initial-scale=1.0" />
       </Head>
       <Header />
       {children}
