986b8a25ee384128788a52f3c396bd685e20d7d3
Mimu-salon <tmimura0@gmail.com>
2021-12-28 16:35:58 +0900

    ✨ Create sidebar using react icons

---
+++ src/components/atomic/molecules/NavItem.tsx
@@ -0,0 +1,42 @@
+import { Flex, Icon } from '@chakra-ui/react';
+import Link from 'next/link';
+import type { ReactText, VFC } from 'react';
+import { memo } from 'react';
+import type { IconType } from 'react-icons';
+
+type Props = {
+  href: string;
+  icon: IconType;
+  children: ReactText;
+};
+export const NavItem: VFC<Props> = memo((props) => {
+  const { href, icon, children } = props;
+  return (
+    <Link href={href} passHref>
+      <Flex
+        align="center"
+        fontSize="sm"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'cyan.400',
+          color: 'white',
+        }}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: 'white',
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
+  );
+});
---
+++ src/components/atomic/organisms/layout/SidebarContent.tsx
@@ -0,0 +1,57 @@
+import { Box, CloseButton, Flex, Text } from '@chakra-ui/react';
+import type { VFC } from 'react';
+import { memo } from 'react';
+import type { IconType } from 'react-icons';
+import { FaDog, FaRegKissBeam, FaRegSurprise } from 'react-icons/fa';
+import { GiSittingDog, GiSniffingDog } from 'react-icons/gi';
+
+import { NavItem } from '../../molecules/NavItem';
+
+type SidebarContentProps = {
+  name: string;
+  icon: IconType;
+  href: string;
+};
+const RecommendItems: Array<SidebarContentProps> = [
+  { name: 'おすすめしたい！', icon: FaRegKissBeam, href: '/' },
+  { name: '似合ってない！？', icon: FaRegSurprise, href: '/about' },
+];
+const WeightItems: Array<SidebarContentProps> = [
+  { name: '小型犬（10kg未満）', icon: FaDog, href: '/' },
+  { name: '中型犬（25kg未満）', icon: GiSittingDog, href: '/about' },
+  { name: '大型犬（25kg以上）', icon: GiSniffingDog, href: '/signin' },
+];
+
+type SidebarProps = {
+  onClose: () => void;
+  display?: { base: string; md: string };
+};
+
+export const SidebarContent: VFC<SidebarProps> = memo((props) => {
+  const { onClose, ...rest } = props;
+  return (
+    <Box borderRight="1px" borderRightColor="gray.200" w={{ base: 'full', md: 60 }} h="full" {...rest}>
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontWeight="bold">
+          おすすめ
+        </Text>
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+      {RecommendItems.map((recommend) => (
+        <NavItem key={recommend.name} href={recommend.href} icon={recommend.icon}>
+          {recommend.name}
+        </NavItem>
+      ))}
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontWeight="bold">
+          大きさ
+        </Text>
+      </Flex>
+      {WeightItems.map((weight) => (
+        <NavItem key={weight.name} href={weight.href} icon={weight.icon}>
+          {weight.name}
+        </NavItem>
+      ))}
+    </Box>
+  );
+});
