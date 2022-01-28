46a0a9925716ed79fc5fb8c7421c9a2b26f43d4c
Mimu-salon <tmimura0@gmail.com>
2022-01-28 17:57:35 +0900

    ✨ Create headerusermenu and set id for menu

--- src/components/atomic/organisms/layout/HeaderLayout.tsx
+++ src/components/atomic/organisms/layout/HeaderLayout.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@chakra-ui/button';
 import { Flex, Heading, Spacer } from '@chakra-ui/layout';
-import { Avatar, Box } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Link from 'next/link';
 import type { VFC } from 'react';
 import { memo } from 'react';
 
+import { HeaderUserMenu } from '../../molecules/HeaderUserMenu';
 import { Notification } from '../../molecules/Notification';
 
 export const HeaderLayout: VFC = memo(() => {
@@ -17,7 +18,7 @@ export const HeaderLayout: VFC = memo(() => {
         <Spacer />
         <Box>
           <Notification isNortification />
-          <Avatar size="md" name="ユーザーの名前" _hover={{ opacity: 0.8 }} src="https://bit.ly/dan-abramov" mr={4} />
+          <HeaderUserMenu />
           <Button colorScheme="green" variant="solid" _hover={{ opacity: 0.8 }}>
             投稿
           </Button>
