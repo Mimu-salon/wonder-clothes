9394f0da8c7bcc3bfb5e8c10d976ef5f55fdefd9
Mimu-salon <tmimura0@gmail.com>
2022-03-09 17:20:58 +0900

    ➕ firebase settings

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp();
+
+export const setCustomClaims = functions.auth.user().onCreate(async (user) => {
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'staff',
+      'x-hasura-allowed-roles': ['staff'],
+      'x-hasura-user-id': user.uid,
+    },
+  };
+  try {
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+    await admin.firestore().collection('user_meta').doc(user.uid).create({
+      refreshTime: admin.firestore.FieldValue.serverTimestamp(),
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+});
---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-default-export */
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/functions';
+
+import firebase from 'firebase/app';
+
+!firebase.apps.length
+  ? firebase.initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    })
+  : firebase.app();
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const strage = firebase.storage();
+export const functions = firebase.functions();
+
+export default firebase;
