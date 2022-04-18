e0441309efa1e2202fdec067cbae978ae6ac8ecc
Mimu-salon <tmimura0@gmail.com>
2022-04-18 17:36:42 +0900

    ✨ Switch header layout with or without on login

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'firebasestorage.googleapis.com',
+      'lh3.googleusercontent.com',
+      'pbs.twimg.com',
+      'blob:http://localhost:3000',
+    ],
+  },
 };
