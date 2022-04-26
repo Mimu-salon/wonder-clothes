68c98bee5c19cea08b1ce9e0793d191c15c85b91
Mimu-salon <tmimura0@gmail.com>
2022-04-26 19:41:17 +0900

    ⚡️ Set reactive variables when reloadiing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,47 @@
 import '../../styles/globals.css';
 
-import { ApolloProvider } from '@apollo/client';
+import { ApolloProvider, useReactiveVar } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
+import { loginUserVar } from '../apollo/cache';
 import { useApollo } from '../apollo/client';
+import type { ReactiveVarGetUserQuery, ReactiveVarGetUserQueryVariables } from '../apollo/graphql';
+import { REACTIVE_VAR_GET_USER } from '../apollo/queries';
 import { theme } from '../components/theme/theme';
+import { auth } from '../firebase/firebaseConfig';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps);
+  const loginUser = useReactiveVar(loginUserVar);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        // ログイン時はグローバルステートをセットするが,ログイン後にURLを直接更新するとリセットされるため、再度セット。
+        if (loginUser === null) {
+          await apolloClient
+            .query<ReactiveVarGetUserQuery, ReactiveVarGetUserQueryVariables>({
+              query: REACTIVE_VAR_GET_USER,
+              variables: {
+                id: user.uid,
+              },
+            })
+            .then((getUser) => {
+              loginUserVar(getUser.data.users_by_pk);
+              // eslint-disable-next-line no-console
+              console.log(loginUserVar());
+            });
+        }
+      } else {
+        // logout時: グローバルステートを初期化
+        loginUserVar(null);
+      }
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ApolloProvider client={apolloClient}>
