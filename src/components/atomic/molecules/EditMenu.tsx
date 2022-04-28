bc899ea4d34b309e9fc5d587b01ef7650c52f4f7
Mimu-salon <tmimura0@gmail.com>
2022-04-28 16:21:54 +0900

    ✨ Create post upload and edit schema

--- src/components/atomic/molecules/EditMenu.tsx
+++ src/components/atomic/molecules/EditMenu.tsx
@@ -1,16 +1,55 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
 import { Menu, MenuButton, MenuItem, MenuList, useDisclosure } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import type { VFC } from 'react';
 import { memo } from 'react';
 
+import { initializeApollo } from '../../../apollo/client';
+import type { DelatePostOneMutation, DelatePostOneMutationVariables, Posts } from '../../../apollo/graphql';
+import { DELETE_POST_ONE } from '../../../apollo/queries';
+import { useMessage } from '../../hooks/useMessage';
+import { deletePostImage } from '../../hooks/usePostContents';
 import { NewPostModal } from './NewPostModal';
 
-export const EditMenu: VFC = memo(() => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+type Props = {
+  post?: Posts;
+};
 
-  const onClickDelete = () => {
-    if (confirm('本当に削除しますか？')) {
-      alert('削除');
+export const EditMenu: VFC<Props> = memo((props) => {
+  const { post } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { showMessage } = useMessage();
+  const client = initializeApollo();
+  const router = useRouter();
+
+  const handleEdit = async () => {
+    if (post) {
+      onOpen();
+    }
+  };
+
+  const handleDelete = async () => {
+    if (post) {
+      if (confirm('本当に削除しますか？')) {
+        if (confirm('本当にこの投稿を削除しますか？')) {
+          const result = await client.mutate<DelatePostOneMutation, DelatePostOneMutationVariables>({
+            mutation: DELETE_POST_ONE,
+            variables: {
+              postId: post.id,
+            },
+          });
+          const deletedPost = result.data?.delete_posts_by_pk;
+          // 削除した投稿に画像が登録されていればfirebase上の画像も削除
+          if (deletedPost?.image && deletedPost.imageUrl) {
+            deletePostImage(deletedPost.user_id, deletedPost.imageUrl);
+          }
+          showMessage({
+            title: '投稿を削除しました',
+            status: 'info',
+          });
+          router.push('/');
+        }
+      }
     }
   };
 
@@ -21,11 +60,11 @@ export const EditMenu: VFC = memo(() => {
           <HamburgerIcon color="gray.600" w="26px" h="26px" />
         </MenuButton>
         <MenuList>
-          <MenuItem onClick={onOpen}>投稿を編集</MenuItem>
-          <MenuItem onClick={onClickDelete}>投稿を削除</MenuItem>
+          <MenuItem onClick={handleEdit}>投稿を編集</MenuItem>
+          <MenuItem onClick={handleDelete}>投稿を削除</MenuItem>
         </MenuList>
         {/* 投稿時のModalを表示 */}
-        <NewPostModal isOpen={isOpen} onClose={onClose} />
+        {post && <NewPostModal isOpen={isOpen} onClose={onClose} postData={post} />}
       </Menu>
     </>
   );
--- src/components/hooks/usePostContents.ts
+++ src/components/hooks/usePostContents.ts
@@ -1,3 +1,37 @@
+import imageCompression from 'browser-image-compression';
+
+import { loginUserVar } from '../../apollo/cache';
+import { initializeApollo } from '../../apollo/client';
+import type {
+  EditPostOneMutation,
+  EditPostOneMutationVariables,
+  EditPostOneWithoutImageMutation,
+  EditPostOneWithoutImageMutationVariables,
+  InsertPostOneMutation,
+  InsertPostOneMutationVariables,
+} from '../../apollo/graphql';
+import { EDIT_POST_ONE, EDIT_POST_ONE_WITHOUT_IMAGE, INSERT_POST_ONE } from '../../apollo/queries';
+import { storage } from '../../firebase/firebaseConfig';
+
+type FromSubmitData = {
+  id?: string; // idがある場合は編集、ない場合は新規投稿
+  image: string | null;
+  imageUrl: string | null;
+  content: string;
+  petName: string;
+  petGender: string;
+  size: string;
+  recommend: string;
+};
+
+// 画像の圧縮
+export const compressFile = async (file: File): Promise<File> => {
+  return await imageCompression(file, {
+    maxSizeMB: 0.5,
+    maxWidthOrHeight: 800,
+  });
+};
+
 // firebase storageに画像を上げる際のfileNameランダム生成
 export const getRandom16DigitsName = () => {
   const S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -9,4 +43,107 @@ export const getRandom16DigitsName = () => {
     .join('');
 
   return fileName;
+};
+
+// 画像をfirebaseにアップロードする
+export const uploadPostImage = async (file: File) => {
+  const loginUser = loginUserVar();
+  const compressedFile = await compressFile(file);
+  const blob = new Blob([compressedFile], { type: file.type });
+
+  const fileName = getRandom16DigitsName();
+  const uploadRef = storage.ref(`images/post/${loginUser?.id}`).child(fileName);
+
+  const image: string = await new Promise((resolve) => {
+    uploadRef
+      .put(blob)
+      .then((snap) => {
+        snap.ref.getDownloadURL().then((downloadURL: string) => {
+          resolve(downloadURL as string);
+        });
+      })
+      .catch((error) => {
+        alert('画像のアップロードに失敗しました');
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  });
+  return {
+    image,
+    imageId: fileName,
+  };
+};
+
+// 投稿が削除された際は、その投稿に登録されている画像も削除する
+export const deletePostImage = (userId: string, fileName: string) => {
+  const imageRef = storage.ref(`images/post/${userId}`).child(fileName);
+  imageRef
+    .delete()
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    .then(() => {})
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    });
+};
+
+// 入力データを元にHasuraのpostsテーブルにINSERTする
+export const insertPostToHasura = async (props: FromSubmitData) => {
+  const { id, image, imageUrl, content, petName, petGender, size, recommend } = props;
+  const client = initializeApollo();
+  const loginUser = loginUserVar();
+
+  if (!loginUser) {
+    return;
+  }
+
+  if (id) {
+    // 投稿編集時
+    if (image && imageUrl) {
+      // 画像変更あり
+      return await client.mutate<EditPostOneMutation, EditPostOneMutationVariables>({
+        mutation: EDIT_POST_ONE,
+        variables: {
+          id: id,
+          user_id: loginUser.id,
+          image: image,
+          imageUrl: imageUrl,
+          content: content,
+          petName: petName,
+          petGender: petGender,
+          //TODO:size, recommendの登録
+        },
+      });
+    } else {
+      // 画像変更なし
+      return await client.mutate<EditPostOneWithoutImageMutation, EditPostOneWithoutImageMutationVariables>({
+        mutation: EDIT_POST_ONE_WITHOUT_IMAGE,
+        variables: {
+          // 投稿編集時
+          id: id,
+          user_id: loginUser.id,
+          content: content,
+          petName: petName,
+          petGender: petGender,
+          //TODO:size, recommendの登録
+        },
+      });
+    }
+  } else {
+    // 新規投稿時
+    return await client.mutate<InsertPostOneMutation, InsertPostOneMutationVariables>({
+      mutation: INSERT_POST_ONE,
+      variables: {
+        user_id: loginUser.id,
+        image: image,
+        imageUrl: imageUrl,
+        content: content,
+        petName: petName,
+        petGender: petGender,
+        post_tag_size: size,
+        post_tag_recommend: recommend,
+        //TODO:size, recommendの登録できているか要確認
+      },
+    });
+  }
 };
