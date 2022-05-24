76abc22a2e90e9e17c1a5d4af10f61257d9d3457
Mimu-salon <tmimura0@gmail.com>
2022-05-25 08:38:22 +0900

    🐛 Fix a Link warning

--- src/components/atomic/organisms/layout/HeaderLayout.tsx
+++ src/components/atomic/organisms/layout/HeaderLayout.tsx
@@ -3,6 +3,7 @@ import { Flex, Heading, Spacer } from '@chakra-ui/layout';
 import { Box, useDisclosure } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import type { VFC } from 'react';
 import { memo } from 'react';
 
@@ -18,6 +19,7 @@ type Props = {
 export const HeaderLayout: VFC<Props> = memo((props) => {
   const { user } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
   const onClickNewPost = () => onOpen();
 
   return (
@@ -25,7 +27,9 @@ export const HeaderLayout: VFC<Props> = memo((props) => {
       <Flex align="center" justify="space-between">
         <Box m={0} p={0} cursor="pointer">
           <Link href="/" passHref>
-            <Image src={'/logo.png'} alt="タイトルロゴ" width="100px" height="70px" />
+            <a href="replace">
+              <Image src={'/logo.png'} alt="タイトルロゴ" width="100px" height="70px" />
+            </a>
           </Link>
         </Box>
         <Spacer />
@@ -40,16 +44,26 @@ export const HeaderLayout: VFC<Props> = memo((props) => {
           </Flex>
         ) : (
           <Box>
-            <Link href="/signup" passHref>
-              <Button colorScheme="green" variant="solid" _hover={{ opacity: 0.8 }} mr={4}>
-                新規会員登録
-              </Button>
-            </Link>
-            <Link href="/signin" passHref>
-              <Button colorScheme="green" variant="outline" bg="white" _hover={{ opacity: 0.8 }}>
-                ログイン
-              </Button>
-            </Link>
+            <Button
+              colorScheme="green"
+              variant="solid"
+              _hover={{ opacity: 0.8 }}
+              mr={4}
+              onClick={() => {
+                router.push('/signup');
+              }}>
+              新規会員登録
+            </Button>
+            <Button
+              colorScheme="green"
+              variant="outline"
+              bg="white"
+              _hover={{ opacity: 0.8 }}
+              onClick={() => {
+                router.push('/signin');
+              }}>
+              ログイン
+            </Button>
           </Box>
         )}
       </Flex>
